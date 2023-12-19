4bf77031b3a96fe34364af9c96f76459d939bd50
RaunaqRaj <raunaqraj2003@gmail.com>
2023-12-19 18:28:55 +1300

    first commit

---
+++ prac_1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+        char string[50];
+        int flag, count = 0;
+
+        cout << "The grammar is: S->aS, S->Sb, S->ab\n";
+        cout << "Enter the string to be checked:\n";
+        cin.getline(string, sizeof(string));
+
+        if(string[0] == 'a')
+    {
+                flag = 0;
+                for (count = 1; string[count - 1] != '\0'; count++)
+        {
+                        if (string[count] == 'b')
+            {
+                                flag = 1;
+                                continue;
+                           
+            }
+            else if ((flag == 1) && (string[count] == 'a'))
+            {
+                cout << "The string does not belong to the specified  grammar" << endl;
+                                break;
+                           
+            }
+            else if (string[count] == 'a')
+            {
+                                continue;
+                           
+            }
+            else if ((flag == 1) && (string[count] == '\0'))
+            {
+                                cout << "String accepted..!!!!" << endl;
+                                break;
+                           
+            }
+            else
+            {
+                cout << "String not accepted" << endl;
+            }
+        }
+    }
+
+     return 0;
+}
---
+++ prac_6.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+const int MAX_RULES = 10;
+
+// Function to find the leading of non-terminals
+void findLeading(char nonTerminal, char* leading, char grammar[][MAX_RULES][MAX_RULES], int ruleCount) {
+    int leadingIndex = 0;
+
+    for (int i = 0; i < ruleCount; ++i) {
+        if (grammar[i][0][0] == nonTerminal) {
+            char symbol = grammar[i][0][2];
+            if (isupper(symbol)) {
+                // If it's a non-terminal, recursively find its leading
+                findLeading(symbol, leading, grammar, ruleCount);
+            } else {
+                // If it's a terminal, add it to the leading array
+                leading[leadingIndex++] = symbol;
+            }
+        }
+    }
+}
+
+int main() {
+    int ruleCount;
+    char grammar[MAX_RULES][MAX_RULES][MAX_RULES];
+
+    cout << "Enter the number of rules: ";
+    cin >> ruleCount;
+
+    cout << "Enter the grammar rules (e.g., S=aA, A=bA, A=ε):" << endl;
+    for (int i = 0; i < ruleCount; ++i) {
+        cin >> grammar[i][0];
+    }
+
+    // Find the leading of non-terminal 'S'
+    char leadingS[MAX_RULES];
+    findLeading('S', leadingS, grammar, ruleCount);
+
+    // Display the result
+    cout << "Leading of non-terminal S: ";
+    for (int i = 0; leadingS[i] != '\0'; ++i) {
+        cout << leadingS[i] << " ";
+    }
+
+    return 0;
+}
